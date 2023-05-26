ae7da9506eed9c77cff108afcdd8b8cfb049d8de
Flopiras <Flopiras@yahoo.it>
2023-05-26 15:08:08 +0200

    add prompt

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ console.log('JS OK')
 
 // 0 Individuare l'elemento del DOM
 const myElement = document.getElementById('price')
+console.log(myElement)
+
+// 1 Chiedere all'utente quanti chilometri intende percorrere
+const userDistance = parseInt(prompt('Quanti chilometri vuoi percorrere?' , 13))
+console.log(userDistance)
+
+// 2 Chiedere all'utente l'età
+const userAge= parseInt(prompt('Quanti anni hai?' , 20))
+console.log(userAge)
