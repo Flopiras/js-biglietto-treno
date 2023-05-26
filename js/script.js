78d4550965ed31f76b2e2681b7b6d2741623b523
Flopiras <Flopiras@yahoo.it>
2023-05-26 15:36:51 +0200

    add discount

--- js/script.js
+++ js/script.js
@@ -7,7 +7,26 @@ console.log(myElement)
 // 1 Chiedere all'utente quanti chilometri intende percorrere
 const userDistance = parseInt(prompt('Quanti chilometri vuoi percorrere?' , 13))
 console.log(userDistance)
+console.log(typeof userDistance)
 
 // 2 Chiedere all'utente l'età
 const userAge= parseInt(prompt('Quanti anni hai?' , 20))
 console.log(userAge)
+console.log(typeof userDistance)
+
+
+// 3 Cacolare il prezzo del biglietto in base ai chilometri
+let lastPrice = 0.21 * userDistance
+console.log(lastPrice)
+console.log(typeof userDistance)
+
+
+// 4 Applicare sconti SE necessari
+
+if(userAge < 18){
+    lastPrice = (lastPrice * 0.8).toFixed(2)
+} else if(userAge > 65){
+    lastPrice= (lastPrice * 0.6).toFixed(2)
+}
+console.log(lastPrice)
+
