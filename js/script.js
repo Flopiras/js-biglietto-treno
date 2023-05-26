60dec42e9e6ecd68050387c4fe450d314de9ee4c
Flopiras <Flopiras@yahoo.it>
2023-05-26 17:08:05 +0200

    fix ; in JS

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-console.log('JS OK')
+console.log('JS OK');
 
 // 0 Individuare l'elemento del DOM
-const myElement = document.getElementById('price')
-console.log(myElement)
+const myElement = document.getElementById('price');
+console.log(myElement);
 
 // 1 Chiedere all'utente quanti chilometri intende percorrere
-const userDistance = parseInt(prompt('Quanti chilometri vuoi percorrere?' , 13))
-console.log(userDistance)
-console.log(typeof userDistance)
+const userDistance = parseInt(prompt('Quanti chilometri vuoi percorrere?' , 13));
+console.log(userDistance);
+console.log(typeof userDistance);
 
 //#VALIDAZIONE
 if (isNaN(userDistance)){
@@ -15,18 +15,18 @@ if (isNaN(userDistance)){
 }
 
 // 2 Chiedere all'utente l'età
-const userAge= parseInt(prompt('Quanti anni hai?' , 20))
-console.log(userAge)
-console.log(typeof userDistance)
+const userAge= parseInt(prompt('Quanti anni hai?' , 20));
+console.log(userAge);
+console.log(typeof userDistance);
 
 if (isNaN(userAge)){
     alert("ATTENZIONE! inserire solo la cifra dei chilometri da percorrere")
 }
 
 // 3 Cacolare il prezzo del biglietto in base ai chilometri
-let lastPrice = 0.21 * userDistance
-console.log(lastPrice)
-console.log(typeof userDistance)
+let lastPrice = 0.21 * userDistance;
+console.log(lastPrice);
+console.log(typeof userDistance);
 
 
 // 4 Applicare sconti SE necessari + 5
@@ -35,7 +35,7 @@ if(userAge < 18){
 } else if(userAge > 65){
     lastPrice= (lastPrice * 0.6).toFixed(2)
 }
-console.log(lastPrice)
+console.log(lastPrice);
 
 // 6 Far apparire il prezzo nel DOM
-myElement.innerText = lastPrice
+myElement.innerText = lastPrice;
